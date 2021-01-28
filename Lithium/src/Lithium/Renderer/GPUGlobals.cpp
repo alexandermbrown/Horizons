f97d7cde1a975c3ab31c5c830f4436b3a630e732
alexandermbrown <alexbr5@bigpond.com>
2021-01-28 15:55:26 +0800

    Add globals to prefer dedicated GPUs

---
+++ Lithium/src/Lithium/Renderer/GPUGlobals.cpp
@@ -0,0 +1,7 @@
+#include "lipch.h"
+
+// Prefer dedicated GPUs.
+extern "C" {
+	_declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+	_declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
